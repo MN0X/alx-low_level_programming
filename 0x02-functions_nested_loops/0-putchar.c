096b309fa02cf2d4959f685fbc1932ac3f0a9117
MN0X <142937146+MN0X@users.noreply.github.com>
2024-06-27 02:21:05 +0100

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,9 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	_putchar('_');
+	int main(void)
+		{
+			_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
